3be414e22ba6f2235a61ec443bb3376ea25d6ec1
BuildTools <unconfigured@null.spigotmc.org>
2020-10-25 15:57:23 -0600

    Cambio identificador lista

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     char opc;
     int dato;
-    Lista_doble *a=new Lista_doble();
+    Lista_doble *lista=new Lista_doble();
     do
     {
         std::cout<<std::endl;
@@ -26,21 +26,21 @@ int main()
         case '1':
             std::cout<<"Escriba el dato a agregar: ";
             std::cin>>dato;
-            a->insertar_dato_inicio(dato);
+            lista->insertar_dato_inicio(dato);
             break;
         case '2':
             std::cout<<"Escriba el dato a agregar: ";
             std::cin>>dato;
-            a->insertar_dato_final(dato);
+            lista->insertar_dato_final(dato);
             break;
         case '3':
-            a->eliminar_dato_inicio();
+            lista->eliminar_dato_inicio();
             break;
         case '4':
-            a->eliminar_dato_final();
+            lista->eliminar_dato_final();
             break;
         case '5':
-            a->mostrar_lista();
+            lista->mostrar_lista();
             break;
         case '6':
             std::cout<<"Saliendo...";
